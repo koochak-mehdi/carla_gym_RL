749f7fc16c460daee95fc8eca8add564e8ee1ec1
mehdi <koochak_mehdi@icloud.com>
2022-01-25 18:35:34 +0100

    Automated commit on 2022-01-25 18:35:34

--- scripts/carla_service/carla_check.sh
+++ scripts/carla_service/carla_check.sh
@@ -4,10 +4,14 @@
 carla_screen="carla_screen"
 counter=0
 DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
-LOG="/home/${USER}/carla_auto_git_log/${DATELOG}.log"
+LOG="/home/${USER}/carla_auto_git_log/${DATELOG}_carla.log"
+
+# commands
+SCREEN=$(which screen)
+PGREP=$(which pgrep)
 
 # check if carla is running
-carla_output=$(pgrep CarlaUE4)
+carla_output=$(${PGREP} CarlaUE4)
 readarray -t carla_PIDS <<< "$carla_output"
 
 # check carla
@@ -18,13 +22,13 @@ then
         if [ ! -n "${carla_PIDS[0]}" ]
         then
             echo "running carla ..." >> ${LOG}
-            screen -dm $carla_screen
-            screen -S $carla_screen -X stuff '/Carla/CARLA_0.9.12/CarlaUE4.sh -RenderOffScreen\n'
+            ${SCREEN} -dm $carla_screen
+            ${SCREEN} -S $carla_screen -X stuff '/Carla/CARLA_0.9.12/CarlaUE4.sh -RenderOffScreen\n'
 
             sleep 10
 
             # check if carla is running
-            carla_output=$(pgrep CarlaUE4)
+            carla_output=$(${PGREP} CarlaUE4)
             readarray -t carla_PIDS <<< "$carla_output"
 
             if [[ ${#carla_PIDS[@]} -le 1 ]]
--- scripts/script_service/script_check.sh
+++ scripts/script_service/script_check.sh
@@ -4,9 +4,19 @@
 mode=$1
 script_screen="script_screen"
 root_script="/Carla/CARLA_0.9.12/PythonAPI/my_scripts/carla_gym_RL/"
+DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
+LOG="/home/${USER}/carla_auto_git_log/${DATELOG}_script.log"
+
+# commands
+GIT=$(which git)
+SCREEN=$(which screen)
+PS=$(which ps)
+GREP=$(which grep)
+AWK=$(which awk)
+
 
 # check if script is running
-script_output=$(ps -aux | grep '[f]irst_try_ddpg' | awk '{print $2}')
+script_output=$(${PS} -aux | ${GREP} '[f]irst_try_ddpg' | ${AWK} '{print $2}')
 readarray -t script_PIDS <<< "$output"
 
 if [[ $mode -eq 1 ]]
@@ -21,31 +31,28 @@ then
 
             if [ ! -n "${script_PIDS[0]}" ]
             then
-                echo "killing screen seesion ..."
-                screen -S $script_screen -X quit
+                echo "killing screen seesion ..." >> ${LOG}
+                ${SCREEN} -S $script_screen -X quit
 
-                echo "running script ..."
-                screen -dm $script_screen
-                screen -S $script_screen -X stuff $'conda activate carla_env\n'
-                screen -S $script_screen -X stuff $"python $root_script/first_try_ddpg.py\n"
+                echo "running script ..." >> ${LOG}
+                ${SCREEN} -dm $script_screen
+                ${SCREEN} -S $script_screen -X stuff $'conda activate carla_env\n'
+                ${SCREEN} -S $script_screen -X stuff $"python $root_script/first_try_ddpg.py\n"
             fi
         fi
     else
-        echo "script is already running ..."
+        echo "script is already running ..." >> ${LOG}
     fi
 
 else
-    echo "mode stop and git"
+    echo "mode stop and git" >> ${LOG}
 
     # kill script screen
-    screen -S $script_screen -X quit
+    ${SCREEN} -S $script_screen -X quit
 
     # do git thigs
     REPO='/Carla/CARLA_0.9.12/PythonAPI/my_scripts/carla_gym_RL'
     COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S'`
-    GIT=$(which git)
-    DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
-    LOG="/home/${USER}/carla_auto_git_log/${DATELOG}.log"
 
     cd ${REPO}
     ${GIT} add --all . >> ${LOG}
