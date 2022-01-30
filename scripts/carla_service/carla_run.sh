5eef21da39ca9eb4544bf3b0982e713dd56331d6
mehdi <koochak_mehdi@icloud.com>
2022-01-30 17:31:19 +0100

    Automated commit on 2022-01-30 17:31:19

--- scripts/carla_service/carla_run.sh
+++ scripts/carla_service/carla_run.sh
@@ -1,7 +1,16 @@
 LOG=$(cat /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/varlog_carla)
 CARLA_SCREEN="carla_screen"
 SCREEN=$(which screen)
+REBOOT=$(which reboot)
 
-/Carla/CARLA_0.9.12/CarlaUE4.sh -RenderOffScreen
+/Carla/CARLA_0.9.12/CarlaUE4.sh -RenderOffScreen > /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/carla_run_var
+
+FLAG=$(cat /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/carla_run_var | grep Segmentation)
+
+if [[ ! -z "${FLAG}" ]] # if is empty
+then
+    sudo ${REBOOT} 0
+fi
+
 sudo systemctl stop carla_check.service
 ${SCREEN} -S ${CARLA_SCREEN} -X quit
--- scripts/script_service/test.sh
+++ scripts/script_service/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo systemctl stop script_check.service
-sudo systemctl stop carla_check.service
-sudo systemctl stop auto_git.service
+ls -la > /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/test_var
+
+FLAG=$(cat /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/test_var | grep mehdi)
+
+if [[ ! -z "${FLAG}" ]] # if is empty
+then
+    echo "salam${FLAG}-"
+else
+    echo "bye${FLAG}-"
+fi
