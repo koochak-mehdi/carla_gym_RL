8a720d1315bad051482a9163d04a7e9eb49d108f
mehdi <koochak_mehdi@icloud.com>
2022-01-30 15:39:45 +0100

    Automated commit on 2022-01-30 15:39:45

--- scripts/script_service/test.sh
+++ scripts/script_service/test.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-LOG=$(cat /home/${USER}/Documents/Mehdi/carla_gym_RL/vars/varlog)
-PYTHON=$(which python)
-SCREEN=$(which screen)
-SCRIPT_SCREEN="script_screen"
-#for i in 0 1 2 3 4 5 6 7 8 9 10
-#do
-#    echo "The date is task1: $(date)" >> ${LOG}
-#    sleep 1
-#done
-${PYTHON} /home/${USER}/Documents/Mehdi/carla_gym_RL/scripts/test.py
-echo "mehdi" >> ${LOG}
 sudo systemctl stop script_check.service
-${SCREEN} -S ${SCRIPT_SCREEN} -X quit
+sudo systemctl stop carla_check.service
+sudo systemctl stop auto_git.service
