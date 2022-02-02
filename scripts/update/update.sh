09dd32e07a84e9fa7163ec988ebb65f2f1f3c0ab
mehdi <koochak_mehdi@icloud.com>
2022-02-03 00:54:56 +0100

    test reboot

--- scripts/update/update.sh
+++ scripts/update/update.sh
@@ -3,3 +3,6 @@
 LOG=$(cat /home/${USER}/Documents/Mehdi/carla_gym_RL/vars/autogit_log)
 
 echo "####################### from update #############################" >> ${LOG}
+sudo reboot 
+sudo reboot 0
+echo 0 > /home/mehdi/Documents/Mehdi/carla_gym_RL/vars/update_flag
