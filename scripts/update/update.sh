9cf2860b04708c7bae73c6c8fdbc50ee807e3910
Mehdi Koochak <koochak_mehdi@icloud.com>
2022-02-02 15:30:38 +0100

    Automated commit from ki on 2022-02-02 15:30:38

--- scripts/update/update.sh
+++ scripts/update/update.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 LOG=$(cat /home/${USER}/Documents/Mehdi/carla_gym_RL/vars/autogit_log)
 
 echo "####################### from update #############################" >> ${LOG}
---
+++ scripts/watch_dog/watch_dog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PS=$(which ps)
+GREP=$(which grep)
+AWK=$(which awk)
+SCREEN=$(which screen)
+
+# check if script is running
+script_output=$(${PS} -aux | ${GREP} '[f]irst_try_ddpg' | ${AWK} '{print $2}')
+
+LOGS=/home/omen_ki_rechner/Documents/Mehdi/carla_gym_RL/logs/simulations
+VAR=/home/omen_ki_rechner/Documents/Mehdi/carla_gym_RL/vars/watchdog_var
+P_WATCHDOG_VAR=$(cat ${VAR})
+SCRIPT_SCREEN="script_screen"
+#echo ${script_output[0]}
+
+if [[ ${#script_output[@]} -eq 1 ]]
+then
+    if [[ ! -z ${script_output[0]} ]]
+    then
+        my_list=$( ls ${LOGS} )
+        echo ${#my_list[@]} > ${VAR}
+        if [[ ${P_WATCHDOG_VAR} -eq ${#my_list[@]} ]]
+        then
+            sleep 30
+            my_list=$( ls ${LOGS} )
+            if [[ ${P_WATCHDOG_VAR} -eq ${#my_list[@]} ]]
+            then
+                sudo systemctl stop script_check.service
+                ${SCREE<N} -S ${SCRIPT_SCREEN} -X quit
+            fi
+        fi
+    fi
+else
+    echo "not running"
+fi
